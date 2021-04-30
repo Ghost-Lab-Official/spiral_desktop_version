6912433aee48fd2908cd396f6d04eec1cb4fc9de
MikeManzi <manzimike378@gmail.com>
2021-04-30 21:40:01 +0200

    Started signup implementation

--- src/pages/Signup.java
+++ src/pages/Signup.java
@@ -15,14 +15,14 @@ public class Signup {
 //    private JLabel location;
 //    private JLabel already;
 //    private JLabel login;
-//    private JLabel spiral;
+    private JLabel spiral;
 //    private JTextField first;
 //    private JTextField last;
 //    private JTextField address;
 //    private JPasswordField pass;
 //    private JTextField place;
     private JPanel titlePanel;
-//    private JPanel formPanel;
+    private JPanel formPanel;
 
 
 
@@ -33,11 +33,21 @@ public class Signup {
         window = new JFrame("Signup Form");
         window.setSize(1360,720);
         window.setLayout(null);
+        window.setBackground(Color.WHITE);
         window.setVisible(true);
         titlePanel = new JPanel();
         titlePanel.setBounds(0,0,400,720);
         titlePanel.setBackground(Color.cyan);
+        spiral = new JLabel("Spiral");
+        spiral.setFont(new Font("Verdana", Font.PLAIN, 30));
+        spiral.setForeground(Color.white);
+        spiral.setBounds(150,300,120,40);
+        formPanel = new JPanel();
+        formPanel.setBounds(400,0,960,720);
+        formPanel.setBackground(Color.white);
+        window.add(spiral);
         window.add(titlePanel);
+        window.add(formPanel);
     }
 
     public static void main(String[] args) throws Exception {
