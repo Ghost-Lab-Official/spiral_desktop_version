684d8c761e6d2b6ac90b1db32fb84e450f8cf633
izereuwonkunda <izereuwonkundakelia@gmail.com>
2021-05-02 19:47:21 +0200

    merging izere and blessing

--- src/ResultDetails.java
+++ src/ResultDetails.java
@@ -61,7 +61,50 @@ public class ResultDetails extends JFrame {
 
         p1.add(lastPanel);
 
+        JLabel resultDetailsTitle, likes, resultDetailsDescription;
+        JPanel descriptionPanel = new JPanel(new GridLayout(4, 4));
+        descriptionPanel.setPreferredSize(new Dimension(400, 480));
+        descriptionPanel.setBackground(Color.BLUE);
+        descriptionPanel.setBorder(BorderFactory.createEmptyBorder(40,40,20,40));
+        p2.setBackground(Color.WHITE);
+        // Initialization of object
+        // "one" of JLabel class.
+        resultDetailsTitle = new JLabel("Amakosi-Ish Kevin");
+        resultDetailsTitle.setFont(new Font("Inter", Font.BOLD,25));
+        likes = new JLabel("Likes: 38.8k");
+        resultDetailsTitle.setBounds(350,30,400,20);
+        likes.setBounds(400,60,80,20);
+        likes.setForeground(Color.decode("#878787"));
+        resultDetailsDescription = new JLabel("<html>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula\n" +
+                "blandit metus eget eleifend. Suspendisse nisl ante, aliquam in nunc at, sagittis\n" +
+                "fringilla lorem. Duis pretium arcu et diam convallis, et lacinia lectus fermentum.\n" +
+                "Nunc ac mattis ante. Nullam et diam efficitur, pulvinar sem ac, congue ipsum.\n" +
+                "Proin lacinia nisi vitae tortor scelerisque fringilla. Fusce in gravida nulla. In quis\n" +
+                "orci ut ex condimentum faucibus nec sed odio. Nulla et turpis mollis, aliquet nib\n" +
+                "id, pharetra massa. Duis finibus ante quis scelerisque luctus.\n" +
+                "\n" +
+                "Mauris vehicula ante vel erat accumsan, eu ultrices elit porttitor. Aliquam accum\n" +
+                "urna nec condimentum suscipit. Fusce eleifend massa cursus elementum pulvin \n" +
+                "Vivamus quis quam luctus, porta metus at, pharetra mi. Quisque diam sapien, p\n" +
+                "uere eu suscipit eu, convallis nec magna. \n" +
+                "\n" +
+                "Cras luctus sagittis feugiat. Vestibului amet dapibus nulla. Integer faucibus id m\n" +
+                "id lectus suscipit suscipit in quis turpis. Sed auctor tempor dolor, vitae placerat\n" +
+                "rhoncus vel. Mauris tellus nisi, congue vitae fringilla id, condimentum vel tellus.\n" +
+                "lentesque ac dui vulputate, ultrices quam sit amet.</html>");
+        
+        resultDetailsDescription.setBounds(230,90,450,250);
+        p2.setLayout(null);
+        p2.add(resultDetailsDescription);
+        // Adding Jlabel "one" on JFrame.
+        p2.add(resultDetailsTitle);
+        p2.add(likes);
+
+
         p1.setLayout (new GridLayout(5,1));
+
+
+
         add(p1, "West");
         add(p2, "East");
         // Function to set visible
