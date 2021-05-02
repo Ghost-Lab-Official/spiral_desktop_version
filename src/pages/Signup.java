da20662d0e665a59cfa3fd697c23fcd61c084db1
MikeManzi <manzimike378@gmail.com>
2021-05-02 10:06:39 +0200

    adjusted the components

--- src/pages/Signup.java
+++ src/pages/Signup.java
@@ -1,7 +1,6 @@
 package pages;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.io.IOException;
@@ -50,53 +49,53 @@ public class Signup {
         formPanel.setBounds(400,0,960,720);
         formPanel.setBackground(Color.white);
         welcome = new JLabel("Welcome to Spiral");
-        welcome.setBounds(510,50,120,20);
+        welcome.setBounds(510,20,120,20);
         welcome.setForeground(Color.decode("#515151"));
         title = new JLabel("Sign Up");
-        title.setBounds(510,90,150,40);
+        title.setBounds(510,40,150,40);
         title.setFont(new Font("verdana", Font.PLAIN, 30));
         title.setForeground(Color.decode("#3674D0"));
         firstName = new JLabel("First name");
-        firstName.setBounds(510,150,150,40);
+        firstName.setBounds(510,100,150,40);
         firstName.setFont(new Font("verdana", Font.PLAIN, 14));
         firstName.setForeground(Color.decode("#202020"));
         first = new JTextField();
-        first.setBounds(510,200,180,40);
+        first.setBounds(510,150,180,40);
         first.setBorder(new LineBorder(Color.BLACK,1,true));
         lastName = new JLabel("Last name");
-        lastName.setBounds(730,150,150,40);
+        lastName.setBounds(730,100,150,40);
         lastName.setFont(new Font("verdana", Font.PLAIN, 14));
         lastName.setForeground(Color.decode("#202020"));
         last = new JTextField();
-        last.setBounds(730,200,180,40);
+        last.setBounds(730,150,180,40);
         last.setBorder(new LineBorder(Color.BLACK,1,true));
         email = new JLabel("Email");
-        email.setBounds(510,250,150,40);
+        email.setBounds(510,200,150,40);
         email.setForeground(Color.decode("#202020"));
         email.setFont(new Font("verdana", Font.PLAIN, 14));
         address = new JTextField();
-        address.setBounds(510,300,400,40);
+        address.setBounds(510,250,400,40);
         address.setBorder(new LineBorder(Color.BLACK,1,true));
         username = new JLabel("Username");
-        username.setBounds(510,350,150,40);
+        username.setBounds(510,300,150,40);
         username.setForeground(Color.decode("#202020"));
         username.setFont(new Font("verdana", Font.PLAIN, 14));
         name = new JTextField();
-        name.setBounds(510,400,400,40);
+        name.setBounds(510,350,400,40);
         name.setBorder(new LineBorder(Color.BLACK,1,true));
         password = new JLabel("Password");
-        password.setBounds(510,450,150,40);
+        password.setBounds(510,400,150,40);
         password.setForeground(Color.decode("#202020"));
         password.setFont(new Font("verdana", Font.PLAIN, 14));
         pass = new JPasswordField();
-        pass.setBounds(510,500,400,40);
+        pass.setBounds(510,450,400,40);
         pass.setBorder(new LineBorder(Color.BLACK,1,true));
         location = new JLabel("Location");
-        location.setBounds(510,550,150,40);
+        location.setBounds(510,500,150,40);
         location.setForeground(Color.decode("#202020"));
         location.setFont(new Font("verdana", Font.PLAIN, 14));
         place = new JTextField();
-        place.setBounds(510,600,400,40);
+        place.setBounds(510,550,400,40);
         place.setBorder(new LineBorder(Color.BLACK,1,true));
 
 
@@ -119,7 +118,6 @@ public class Signup {
         window.add(titlePanel);
         window.add(formPanel);
         window.setVisible(true);
-
     }
 
     public static void main(String[] args) throws Exception {
