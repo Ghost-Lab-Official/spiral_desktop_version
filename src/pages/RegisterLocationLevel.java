26e38f73726c5b8bc359dd0b6cdddbeb0364a97e
phelixdusengimana <phelixdusengimana@gmail.com>
2021-05-03 14:44:25 +0200

    added desgin in the location level

--- src/pages/RegisterLocationLevel.java
+++ src/pages/RegisterLocationLevel.java
@@ -1,6 +1,7 @@
 package pages;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class RegisterLocationLevel extends JFrame {
@@ -18,6 +19,14 @@ public class RegisterLocationLevel extends JFrame {
         JPanel leftPanel = new JPanel();
         JPanel rightPanel = new JPanel();
         JLabel spiralLabel = new JLabel("Spiral");
+        JButton createBtn = new JButton("Register");
+        JLabel levelText = new JLabel("<html>This is where you could register your?<br/> <font color='#3674D0'>Sign Up.</font></html>");
+        JPanel CreateButtonPanel = new JPanel(new BorderLayout());
+        JPanel DescriptLabelPanel = new JPanel();
+
+//        company name
+        spiralLabel.setFont(new Font("Nunito", Font.BOLD, 40));
+        spiralLabel.setForeground(Color.WHITE);
 
 //        left panel
         leftPanel.setBackground(themeColor);
@@ -26,7 +35,26 @@ public class RegisterLocationLevel extends JFrame {
         leftPanel.setLayout(new GridBagLayout());
 
 
+        DescriptLabelPanel.setBackground(Color.WHITE);
+        DescriptLabelPanel.setLayout(new BorderLayout());
+        DescriptLabelPanel.add(levelText, BorderLayout.SOUTH);
+
 //      right panel
+        createBtn.setPreferredSize(new Dimension(150,50));
+        createBtn.setForeground(Color.WHITE);
+        createBtn.setBackground(themeColor);
+        createBtn.setFont(new Font("Nunito",Font.BOLD,16));
+        createBtn.setBounds(10, 10, 120, 50);
+        createBtn.setBorder(BorderFactory.createCompoundBorder(
+                new CustomBorder(),
+                new EmptyBorder(new Insets(25, 20, 25, 25))
+        ));
+
+        CreateButtonPanel.setBackground(Color.WHITE);
+        CreateButtonPanel.add(createBtn, BorderLayout.WEST);
+
+        rightPanel.add(CreateButtonPanel);
+        rightPanel.add(DescriptLabelPanel);
         rightPanel.setLayout(new GridLayout(9,1, 0, 10));
         rightPanel.setBackground(Color.WHITE);
         rightPanel.setBorder(BorderFactory.createEmptyBorder(30, 120, 30, 120));
