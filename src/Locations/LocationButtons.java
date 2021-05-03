eedc4aa3d03b3e149b619e264569128a8ca78ae7
pauline12ish34 <paulineishimwe12@gmail.com>
2021-05-03 17:24:32 +0200

    changes

--- src/Locations/LocationButtons.java
+++ src/Locations/LocationButtons.java
@@ -16,47 +16,50 @@ public class LocationButtons {
     public LocationButtons() {
         LocationsInit();
     }
-    public void LocationsInit(){
-       window= new JFrame("Location buttons");
-        window.setSize(1375,735);
+
+    public void LocationsInit() {
+        window = new JFrame("Location buttons");
+        window.setSize(1375, 735);
         window.setLayout(null);
-        window.setBackground(Color.WHITE);
+        window.setBackground(Color.decode("#E8F0FE"));
 
         panel1 = new JPanel();
-        panel1.setBounds(0,0,1375,732);
-      panel1.setBackground(Color.decode("#E8F0FE"));
+        panel1.setBounds(0, 50, 1375, 32);
 
-        titleView= new JLabel("View Locations",JLabel.NORTH_EAST);
-        titleView.setFont(new Font("Unto",Font.BOLD,20));
+        titleView = new JLabel("View Locations", JLabel.NORTH_EAST);
+        titleView.setFont(new Font("Nunito", Font.BOLD, 20));
         titleView.setForeground(Color.decode("#3674D0"));
-       titleView.setBounds(0,25,15,3);
+        titleView.setBounds(0, 25, 15, 3);
 
 
-        levelButton= new JButton("+ Add Level");
-        levelButton.setBounds(10,25,25,22);
+        levelButton = new JButton("+ Add Level");
+//        levelButton.setBounds(10, 25, 55, 52);
         levelButton.setBackground(Color.decode("#CCCCCD"));
+        levelButton.setFont(new Font("Nunito",Font.PLAIN,17));
         levelButton.setForeground(Color.decode("#323232"));
+        levelButton.setBorder(null);
+        levelButton.setFocusPainted(false);
 
 
 
-        locationButton= new JButton("+ Add Location");
-        locationButton.setBounds(15,25,25,22);
+        locationButton = new JButton("+ Add Location");
+        locationButton.setBounds(15, 25, 0, 0);
+        locationButton.setSize(new Dimension(40,90));
         locationButton.setBackground(Color.decode("#3674D0"));
         locationButton.setForeground(Color.WHITE);
 
 
-
-panel1.add(titleView);
-panel1.add(Box.createHorizontalStrut(500));
-panel1.add(levelButton);
-panel1.add(locationButton);
-window.add(panel1);
+        panel1.add(titleView);
+        panel1.add(Box.createHorizontalStrut(700));
+        panel1.add(levelButton);
+        panel1.add(locationButton);
+        window.add(panel1);
 
         window.setVisible(true);
     }
 
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         new LocationButtons();
     }
 }
